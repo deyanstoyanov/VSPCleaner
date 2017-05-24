3606c24eadf480d02a1a3d589d2563d4d4ea5a7a
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-05-24 11:14:30 +0300

    Implemented custom DelegateCommand

---
+++ VSPCleaner/VSPCleaner/Infrastructure/Commands/DelegateCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace VSPCleaner.Infrastructure.Commands
+{
+    using System;
+    using System.Windows.Input;
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action action;
+
+        public DelegateCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public void Execute(object parameter) => this.action();
+
+        public bool CanExecute(object parameter) => true;
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
