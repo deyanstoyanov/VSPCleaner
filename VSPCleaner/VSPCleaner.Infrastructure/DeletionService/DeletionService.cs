83bf12ce32ae5e7b12da30cb3f6c2502a9ec95f0
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-05-24 22:17:20 +0300

    Implemented DeletionService with two method for deleting directories

---
+++ VSPCleaner/VSPCleaner.Infrastructure/DeletionService/DeletionService.cs
@@ -0,0 +1,55 @@
+﻿namespace VSPCleaner.Infrastructure.DeletionService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class DeletionService
+    {
+        public static void DeleteFoldersRecursively(string directory)
+        {
+            string[] directories;
+            try
+            {
+                directories = Directory.GetDirectories(directory);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return;
+            }
+
+            foreach (var currentDirectory in directories)
+            {
+                var folder = currentDirectory.Substring(
+                    currentDirectory.LastIndexOf(@"\", StringComparison.Ordinal) + 1);
+                if (folder.Equals("bin", StringComparison.OrdinalIgnoreCase)
+                    || folder.Equals("obj", StringComparison.OrdinalIgnoreCase))
+                {
+                    try
+                    {
+                        DeleteFolder(currentDirectory);
+                    }
+                    catch (IOException)
+                    {
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                    }
+                }
+                else
+                {
+                    DeleteFoldersRecursively(currentDirectory);
+                }
+            }
+        }
+
+        public static void DeleteFolder(string directory)
+        {
+            if (Directory.Exists(directory))
+            {
+                Directory.Delete(directory, true);
+            }
+        }
+    }
+}
