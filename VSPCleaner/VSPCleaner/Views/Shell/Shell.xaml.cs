0339eeb1b39d34f452a0924198fa7a9e819a39a5
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-05-24 10:08:04 +0300

    Created a default Shell window

---
+++ VSPCleaner/VSPCleaner/Views/Shell/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace VSPCleaner.Views.Shell
+{
+    using System.Windows.Controls.Ribbon;
+
+    /// <summary>
+    /// Interaction logic for Shell.xaml
+    /// </summary>
+    public partial class Shell : RibbonWindow
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
