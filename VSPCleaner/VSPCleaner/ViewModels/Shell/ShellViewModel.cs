c8b9ded7090925a175b3c242bca00c8ba489ddeb
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-05-24 11:15:06 +0300

    Implemented ExitCommand

--- VSPCleaner/VSPCleaner/ViewModels/Shell/ShellViewModel.cs
+++ VSPCleaner/VSPCleaner/ViewModels/Shell/ShellViewModel.cs
@@ -1,6 +1,19 @@
 ﻿namespace VSPCleaner.ViewModels.Shell
 {
+    using System.Windows;
+    using System.Windows.Input;
+
+    using VSPCleaner.Infrastructure.Commands;
+
     public class ShellViewModel
     {
+        public ShellViewModel()
+        {
+            this.ExitCommand = new DelegateCommand(this.OnExitCommand);
+        }
+
+        public ICommand ExitCommand { get; set; }
+
+        private void OnExitCommand() => Application.Current.Shutdown();
     }
 }
