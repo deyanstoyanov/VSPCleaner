a7614359b68465b61b52353996e02621ef538f0a
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-05-24 22:28:12 +0300

    Global exception handler
    Trap unhandled exceptions at different levels:
    From all threads in the AppDomain.
    From a single specific UI dispatcher thread.
    From the main UI dispatcher thread.
    From within each AppDomain that uses a task scheduler for asynchronous operations.

--- VSPCleaner/VSPCleaner/App.xaml.cs
+++ VSPCleaner/VSPCleaner/App.xaml.cs
@@ -1,11 +1,51 @@
 ﻿namespace VSPCleaner
 {
+    using System;
+    using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Threading;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += this.CurrentDomainOnUnhandledException;
+            this.DispatcherUnhandledException += this.OnDispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += this.TaskSchedulerOnUnobservedTaskException;
+        }
+
+        private void TaskSchedulerOnUnobservedTaskException(
+            object sender, 
+            UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+        {
+            HandleException(unobservedTaskExceptionEventArgs.Exception);
+        }
+
+        private void OnDispatcherUnhandledException(
+            object sender, 
+            DispatcherUnhandledExceptionEventArgs dispatcherUnhandledExceptionEventArgs)
+        {
+            HandleException(dispatcherUnhandledExceptionEventArgs.Exception);
+            dispatcherUnhandledExceptionEventArgs.Handled = true;
+        }
+
+        private void CurrentDomainOnUnhandledException(
+            object sender, 
+            UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            Exception exception = unhandledExceptionEventArgs.ExceptionObject as Exception;
+            HandleException(exception);
+        }
+
+        private static void HandleException(Exception exception)
+        {
+            MessageBox.Show(exception.Message);
+        }
     }
 }
